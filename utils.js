898f61f1d00a1f0186a4302cf1246b69feadf23c
Adam Szczepinski <adam.szczepinski@przelewy24.pl>
2024-03-05 23:35:36 +0100

    add checkbox

--- utils.js
+++ utils.js
@@ -10,22 +10,41 @@ const waitForEl = async (selector, duration) => {
     await sleep(duration);
     return document.querySelector(selector) || waitForEl(selector, duration);
 }
-//zwróć select z options
-const select = (values, hadleChange, style) => {
-    const select = document.createElement('select');
-    Object.assign(select.style, style);
-    const optionDef = document.createElement('option');
-    optionDef.selected = true;
-    optionDef.disabled = true;
-    optionDef.value = -1;
-    optionDef.innerText = 'wybierz..';
-    select.appendChild(optionDef);
-    values.forEach(({ value, text }) => {
-        const option = document.createElement('option');
-        option.value = value;
-        option.innerText = text||value;
-        select.appendChild(option);
-    })
-    select.addEventListener('change', hadleChange)
-    return select;
+//gotowe elementy HTML
+const element = () => {
+    //zwróć select z options
+    const select = (values, hadleChange, style) => {
+        const select = document.createElement('select');
+        Object.assign(select.style, style);
+        const optionDef = document.createElement('option');
+        optionDef.selected = true;
+        optionDef.disabled = true;
+        optionDef.value = -1;
+        optionDef.innerText = 'wybierz..';
+        select.appendChild(optionDef);
+        values.forEach(({ value, text }) => {
+            const option = document.createElement('option');
+            option.value = value;
+            option.innerText = text || value;
+            select.appendChild(option);
+        })
+        select.addEventListener('change', hadleChange)
+        return select;
+    }
+    //zwróć grupę checkboxów,
+    const checkbox = (values, hadleChange) => {
+        const div = document.createElement('div');
+        values.forEach(({ value, text }) => {
+            const lbl = document.createElement('label');
+            lbl.innerText = text || value;
+            const radio = document.createElement('input');
+            radio.value = value;
+            radio.type = 'checkbox';
+            radio.addEventListener('change', hadleChange);
+            lbl.prepend(radio);
+            div.appendChild(lbl);
+        })
+        return div;
+    }
+    return { select, checkbox }
 }
