61a92f75e9adbf9646cd43d319a926c112b2c69f
Adam Szczepinski <adam.szczepinski@przelewy24.pl>
2024-03-05 23:22:04 +0100

    add select

--- utils.js
+++ utils.js
@@ -6,7 +6,26 @@ const waitForElAll = async (selector, duration) => {
     return document.querySelectorAll(selector).length ? document.querySelectorAll(selector) : waitForElAll(selector, duration);
 }
 //czekaj na element
-const waitForEl = async (selector,duration) => {
+const waitForEl = async (selector, duration) => {
     await sleep(duration);
-    return document.querySelector(selector) || waitForEl(selector,duration);
+    return document.querySelector(selector) || waitForEl(selector, duration);
+}
+//zwróć select z options
+const select = (values, hadleChange, style) => {
+    const select = document.createElement('select');
+    Object.assign(select.style, style);
+    const optionDef = document.createElement('option');
+    optionDef.selected = true;
+    optionDef.disabled = true;
+    optionDef.value = -1;
+    optionDef.innerText = 'wybierz..';
+    select.appendChild(optionDef);
+    values.forEach(({ value, text }) => {
+        const option = document.createElement('option');
+        option.value = value;
+        option.innerText = text||value;
+        select.appendChild(option);
+    })
+    select.addEventListener('change', hadleChange)
+    return select;
 }
